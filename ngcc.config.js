e92da017887331c64ff81fa04b7febc39cde656c
Gaurav Sharma <codergauravsharma@users.noreply.github.com>
2020-06-26 23:34:10 +0530

    Trying to fix "Zone already loaded" error, no luck so far.

--- ngcc.config.js
+++ ngcc.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  packages: {
-    "@nativescript/angular": {
-      entryPoints: {
-        ".": {
-          override: {
-            main: "./index.js",
-            typings: "./index.d.ts",
-          },
-          ignoreMissingDependencies: true,
-        }
-      },
-      ignorableDeepImportMatchers: [
-        /tns-core-modules\//,
-        /@nativescript\/core\//,
-      ]
-    }
-  }
+	packages: {
+		"@nativescript/angular": {
+			entryPoints: {
+				".": {
+					override: {
+						main: "./index.js",
+						typings: "./index.d.ts",
+					},
+					ignoreMissingDependencies: true,
+				},
+			},
+			ignorableDeepImportMatchers: [
+				/zone.js\//,
+				/tns-core-modules\//,
+				/@nativescript\/core\//,
+			],
+		},
+	},
 };
