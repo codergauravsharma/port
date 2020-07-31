27a29dece0e180fa41d61e02de9cae1a9141ea42
Gaurav Sharma <codergauravsharma@users.noreply.github.com>
2020-08-01 01:13:04 +0530

    trying bottom navigation

--- src/app/root/app.common.ts
+++ src/app/root/app.common.ts
@@ -1,38 +1,12 @@
 import { Routes } from '@angular/router';
 import { Provider } from '@angular/core';
 import { AppRootComponent } from './components/app-root/app-root.component';
-import { AppHubComponent } from './components/app-hub/app-hub.component';
 
 export const componentDeclarations: any[] = [
-	AppRootComponent, AppHubComponent
+	AppRootComponent
 ];
 
 export const providerDeclarations: Provider[] = [];
 
 export const routes: Routes = [
-	// Option 1 - README.md
-	// { path: '', redirectTo: 'app-hub', pathMatch: 'full' },
-	// {
-	// 	path: 'app-hub',
-	// 	loadChildren: () =>
-	// 		import('../features/app-hub/app-hub.module').then(
-	// 			(module) => module.AppHubModule
-	// 		),
-	// }
-
-	// Option 2 - README.md
-	// {
-	// 	path: '',
-	// 	component: AppHubComponent,
-	// 	children: [
-	// 		{ path: '', redirectTo: 'landing-zone', pathMatch: 'full' },
-	// 		{
-	// 			path: 'landing-zone',
-	// 			loadChildren: () =>
-	// 				import('../features/landing-zone/landing-zone.module').then(
-	// 					module => module.LandingZoneModule
-	// 				)
-	// 		}
-	// 	]
-	// }
 ];
